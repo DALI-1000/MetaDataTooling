deaf4de8025c477593105adc15e6e36adb56b336
Torsten Lubomirski <torsten.lubomirski@outlook.com>
2017-12-03 20:33:23 +0100

    save work

--- MetaDataTooling/MetaDataTooling/Program.cs
+++ MetaDataTooling/MetaDataTooling/Program.cs
@@ -3,17 +3,19 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace MetaDataTooling
 {
     class Program
     {
+        private const string GUID_REGEX = @"(\{){0,1}[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}(\}){0,1}";
         static void Main(string[] args)
         {
             var path = Console.ReadLine();
             if (path == "")
-                path = @"C:\Users\torst\Desktop\vt_datamanagement_src";
+                path = @"E:\vt_datamanagement_src";
             while (!Directory.Exists(path))
             {
                 Console.WriteLine("Path not found. Please retry...");
@@ -26,13 +28,24 @@ namespace MetaDataTooling
             {
                 using (StreamReader sr = new StreamReader(Path.Combine(path, sln), Encoding.Default))
                 {
-                    var content = sr.ReadToEnd();
+                    var content = sr.ReadToEnd().Replace("\t", "")
+                        .Replace("\r", "").Replace("\n", "").Replace("\"", "");
                     var projects = content.Split(new string[1] { "Project" },
-                        StringSplitOptions.RemoveEmptyEntries)[1].Split('=')[1].Split(',');
-                    for (int i = 0; i < projects.Length; i++)
-                        projects[i] = projects[i].Replace("\"", "").Replace("\r", "")
-                            .Replace("\n", "").Replace("End", "");
-                    slnDict.Add(sln, projects.ToList());
+                        StringSplitOptions.RemoveEmptyEntries).ToList();
+                    //for (int i = 0; i < projects.Count; i++)
+                    //    projects[i] = "Project_" + projects[i];
+                    //projects.RemoveAll(x => !(x.StartsWith("Project_") && x.EndsWith("End")));
+                    projects.RemoveAll(x => !(Regex.Matches(x,
+                        GUID_REGEX).Count == 2));
+                    projects.RemoveAll(x => !(x.Contains(".csproj")));
+                    for (int i = 0; i < projects.Count; i++)
+                    {
+                        projects[i] = projects[i].Replace("\n", "").Replace("\r", "");
+                        var help = projects[i].Split('=');
+                        projects[i] = help[0] + "|" + help[1].Split(',').ToList()
+                            .ConvertAll<string>(x => x[0] + "|" + x[1] + "|" + x[2]);
+                    }
+                    
                 }
             }
         }
